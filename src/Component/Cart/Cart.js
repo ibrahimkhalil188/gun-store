9a5bd9b6f0863d4acc1d36f9e8b10868131ae422
unknown <ibrahimkhalil.mine@gmail.com>
2022-03-24 12:55:55 +0600

    tex and grand total

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -3,19 +3,19 @@ import './Cart.css'
 
 const Cart = ({ cart }) => {
     let total = 0
-    let image = []
+    let tex = 0;
     for (const element of cart) {
         total = total + parseFloat(element.price)
-        image = [...image, element.img]
+        tex = tex + parseFloat(element.price * 0.1)
         console.log(element)
     }
     return (
         <div className='cart'>
             <h2>Order Summary</h2>
-
             <p>Selected item: {cart.length}</p>
             <p>Total Price: ${total}</p>
-            <img className='image-cart' src={image} alt="" />
+            <p>Total Tex: ${tex}</p>
+            <h4>Grand Total: ${total + tex}</h4>
         </div>
     );
 };
